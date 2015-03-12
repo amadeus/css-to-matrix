3e2de0e3a5a14fc227bf19b8e4c64fe40b5939f2
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2015-03-12 12:45:58 -0700

    Adding tests for setMatrixFromCSS

--- test/test.coffee.md
+++ test/test.coffee.md
@@ -17,35 +17,27 @@ setup
 	describe 'constructor', ->
 
 		it 'adds data that is passed when intialized to its model', ->
-
 			matrixee = new Matrixee data
 			actual = matrixee.model.get 'matrix'
-
 			expect(actual).to.deep.equal data
 
 	describe 'matrix', ->
 
-		it 'should add valid data to its instance model', ->
-
+		it 'add valid data to its instance model', ->
 			matrixee = new Matrixee
 			matrixee.matrix data
 			actual = matrixee.model.get 'matrix'
-
 			expect(actual).to.deep.equal data
 
-		it 'should throw an error when intialized with an invalid array', ->
-
+		it 'throw an error when intialized with an invalid array', ->
 			fn = -> new Matrixee 'bad'
-
 			expect(fn).to.throw Error
 
 	describe 'getMatrix', ->
 
-		it 'should properly apply transformations', ->
-
+		it 'properly apply transformations', ->
 			matrixee = new Matrixee
 			matrixee.translate3d 10, 20, 30
-
 			actual = matrixee.getMatrix()
 			expected = [
 				[1, 0, 0, 0]
@@ -53,17 +45,64 @@ setup
 				[0, 0, 1, 0]
 				[10, 20, 30, 1]
 			]
-
 			expect(actual).to.deep.equal expected
 
-	describe 'getMatrixCSS', ->
+	describe 'getMatrixCSS-durp', ->
 
-		it 'should convert matricies to CSS strings', ->
-
+		it 'convert matricies to CSS strings', ->
 			matrixee = new Matrixee data
 			css = matrixee.getMatrixCSS()
-
 			expect(css).to.equal 'matrix3d(1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16)'
+
+	describe 'setMatrixFromCSS', ->
+
+		it 'set a 4x4 matrix from a matrix3d css string', ->
+			matrixee = new Matrixee
+			matrixee.setMatrixFromCSS 'matrix3d(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)'
+			actual = matrixee.getMatrix()
+			expected = [
+				[0, 4, 8,  12],
+				[1, 5, 9,  13],
+				[2, 6, 10, 14],
+				[3, 7, 11, 15]
+			]
+			expect(actual).to.deep.equal expected
+
+		it 'set a 4x4 matrix from a matrix 2d css string', ->
+			matrixee = new Matrixee
+			matrixee.setMatrixFromCSS 'matrix(0, 1, 2, 3, 4, 5)'
+			actual = matrixee.getMatrix()
+			expected = [
+				[0, 2, 4, 0],
+				[1, 3, 5, 0],
+				[0, 0, 1, 0],
+				[0, 0, 0, 1]
+			]
+			expect(actual).to.deep.equal expected
+
+		it 'create an identity matrix with a none value', ->
+			matrixee = new Matrixee
+			matrixee.setMatrixFromCSS('none')
+			actual = matrixee.getMatrix()
+			expected = [
+				[1, 0, 0, 0],
+				[0, 1, 0, 0],
+				[0, 0, 1, 0],
+				[0, 0, 0, 1]
+			]
+			expect(actual).to.deep.equal expected
+
+		it 'create an identity matrix from an empty string', ->
+			matrixee = new Matrixee
+			matrixee.setMatrixFromCSS()
+			actual = matrixee.getMatrix()
+			expected = [
+				[1, 0, 0, 0],
+				[0, 1, 0, 0],
+				[0, 0, 1, 0],
+				[0, 0, 0, 1]
+			]
+			expect(actual).to.deep.equal expected
 
 run
 
--- test/test.js
+++ test/test.js
@@ -21,14 +21,14 @@
   });
 
   describe('matrix', function() {
-    it('should add valid data to its instance model', function() {
+    it('add valid data to its instance model', function() {
       var actual, matrixee;
       matrixee = new Matrixee;
       matrixee.matrix(data);
       actual = matrixee.model.get('matrix');
       return expect(actual).to.deep.equal(data);
     });
-    return it('should throw an error when intialized with an invalid array', function() {
+    return it('throw an error when intialized with an invalid array', function() {
       var fn;
       fn = function() {
         return new Matrixee('bad');
@@ -38,7 +38,7 @@
   });
 
   describe('getMatrix', function() {
-    return it('should properly apply transformations', function() {
+    return it('properly apply transformations', function() {
       var actual, expected, matrixee;
       matrixee = new Matrixee;
       matrixee.translate3d(10, 20, 30);
@@ -48,12 +48,47 @@
     });
   });
 
-  describe('getMatrixCSS', function() {
-    return it('should convert matricies to CSS strings', function() {
+  describe('getMatrixCSS-durp', function() {
+    return it('convert matricies to CSS strings', function() {
       var css, matrixee;
       matrixee = new Matrixee(data);
       css = matrixee.getMatrixCSS();
       return expect(css).to.equal('matrix3d(1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16)');
+    });
+  });
+
+  describe('setMatrixFromCSS', function() {
+    it('set a 4x4 matrix from a matrix3d css string', function() {
+      var actual, expected, matrixee;
+      matrixee = new Matrixee;
+      matrixee.setMatrixFromCSS('matrix3d(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)');
+      actual = matrixee.getMatrix();
+      expected = [[0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]];
+      return expect(actual).to.deep.equal(expected);
+    });
+    it('set a 4x4 matrix from a matrix 2d css string', function() {
+      var actual, expected, matrixee;
+      matrixee = new Matrixee;
+      matrixee.setMatrixFromCSS('matrix(0, 1, 2, 3, 4, 5)');
+      actual = matrixee.getMatrix();
+      expected = [[0, 2, 4, 0], [1, 3, 5, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+      return expect(actual).to.deep.equal(expected);
+    });
+    it('create an identity matrix with a none value', function() {
+      var actual, expected, matrixee;
+      matrixee = new Matrixee;
+      matrixee.setMatrixFromCSS('none');
+      actual = matrixee.getMatrix();
+      expected = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+      return expect(actual).to.deep.equal(expected);
+    });
+    return it('create an identity matrix from an empty string', function() {
+      var actual, expected, matrixee;
+      matrixee = new Matrixee;
+      matrixee.setMatrixFromCSS();
+      actual = matrixee.getMatrix();
+      expected = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+      return expect(actual).to.deep.equal(expected);
     });
   });
 
